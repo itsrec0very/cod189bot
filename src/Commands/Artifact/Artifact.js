cc7d2bb4905a0321cdb8bff5c654d8102df2d75d
ItsRecovery <recoverydyne.original@gmail.com>
2023-12-11 23:12:38 +0300

    Update Artifact.js

--- src/Commands/Artifact/Artifact.js
+++ src/Commands/Artifact/Artifact.js
@@ -1,58 +1,72 @@
-const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  AttachmentBuilder,
+} = require("discord.js");
 const artifacts = require("../../../contents/artifacts.json");
 
 module.exports = {
-    cooldown: 5,
-    data: new SlashCommandBuilder()
-        .setName("artifact")
-        .setDescription("Get information about an artifact")
-        .setDMPermission(false)
-        .addStringOption(option =>
-            option
-                .setName("name")
-                .setDescription("Name of the artifact")
-                .setRequired(true)
-                .setAutocomplete(true)
-        ),
+  cooldown: 5,
+  data: new SlashCommandBuilder()
+    .setName("артефакт")
+    .setDescription("Получить информацию об артефакте")
+    .setDMPermission(false)
+    .addStringOption((option) =>
+      option
+        .setName("name")
+        .setDescription("Название артефакта")
+        .setRequired(true)
+        .setAutocomplete(true),
+    ),
 
-    async autocomplete(interaction) {
-        const focusedValue = interaction.options.getFocused().toLowerCase();
-        const choices = Object.keys(artifacts);
-        const filtered = choices.filter(choice => choice.toLowerCase().startsWith(focusedValue)).slice(0, 25);
-        await interaction.respond(filtered.map(choice => ({ name: choice, value: choice })));
-    },
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused().toLowerCase();
+    const choices = Object.keys(artifacts);
+    const filtered = choices
+      .filter((choice) => choice.toLowerCase().startsWith(focusedValue))
+      .slice(0, 25);
+    await interaction.respond(
+      filtered.map((choice) => ({ name: choice, value: choice })),
+    );
+  },
 
-    async execute(interaction) {
-        const userSelection = interaction.options.getString("name");
+  async execute(interaction) {
+    const userSelection = interaction.options.getString("name");
 
-        const selectedArtifact = artifacts[userSelection];
+    const selectedArtifact = artifacts[userSelection];
 
-        const embed = new EmbedBuilder()
-            .setColor("#f59e0b")
-            .setTitle(userSelection)
-            .setFields(
-                { name: "Rarity:", value: selectedArtifact.rarity, inline: false },
-                { name: "Role:", value: selectedArtifact.role, inline: false },
-                { name: "Buffs:", value: selectedArtifact.buffs, inline: false },
-                { name: "Units:", value: selectedArtifact.units, inline: false },
-                { name: "Tier:", value: selectedArtifact.tier, inline: false }
-            );
+    const embed = new EmbedBuilder()
+      .setColor("#f59e0b")
+      .setTitle(userSelection)
+      .setFields(
+        { name: "Редкость:", value: selectedArtifact.rarity, inline: false },
+        {
+          name: "Предназначение:",
+          value: selectedArtifact.role,
+          inline: false,
+        },
+        { name: "Баф:", value: selectedArtifact.buffs, inline: false },
+        {
+          name: "Роль:",
+          value: selectedArtifact.units,
+          inline: false,
+        },
+        { name: "Уровень:", value: selectedArtifact.tier, inline: false },
+      );
 
-        if (selectedArtifact.image !== "") {
+    if (selectedArtifact.image !== "") {
+      const attachment = new AttachmentBuilder()
+        .setFile(`./assets/artifact/${selectedArtifact.image}`)
+        .setName(`image.png`);
 
-            const attachment = new AttachmentBuilder()
-                .setFile(`./assets/artifact/${selectedArtifact.image}`)
-                .setName(`image.png`)
+      const embed2 = new EmbedBuilder()
+        .setColor("#f59e0b")
+        .setTitle(`${userSelection}`)
+        .setImage("attachment://image.png");
 
-            const embed2 = new EmbedBuilder()
-                .setColor("#f59e0b")
-                .setTitle(`${userSelection}`)
-                .setImage("attachment://image.png")
-
-            await interaction.reply({ files: [attachment], embeds: [embed2, embed] });
-        } else {
-            await interaction.reply({ embeds: [embed] });
-        }
-
-    },
+      await interaction.reply({ files: [attachment], embeds: [embed2, embed] });
+    } else {
+      await interaction.reply({ embeds: [embed] });
+    }
+  },
 };
